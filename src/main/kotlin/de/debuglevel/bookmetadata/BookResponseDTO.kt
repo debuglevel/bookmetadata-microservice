fc94ad602687fda81118d84a6e63094759bec687
Marc Kohaupt <debuglevel@gmail.com>
2019-08-27 21:45:03 +0200

    Use data class with all properties to enable proper equals() and toString()

--- src/main/kotlin/de/debuglevel/bookmetadata/BookResponseDTO.kt
+++ src/main/kotlin/de/debuglevel/bookmetadata/BookResponseDTO.kt
@@ -1,23 +1,24 @@
 package de.debuglevel.bookmetadata
 
-data class BookResponseDTO(var isbn: String?) {
-    var requestedIsbn: String? = null
-    var title: String? = null
-    var subtitle: String? = null
-    var combinedTitle: String? = null
-    var author: String? = null
-    var year: String? = null
-    var edition: String? = null
-    var publisher: String? = null
-    var place: String? = null
-    var description: String? = null
-    var source: String? = null
-    var series: String? = null
-    var volume: String? = null
-    var price: String? = null
-    var pages: String? = null
-    var tableOfContentsUrl: String? = null
-    var abstractUrl: String? = null
-    var abstract: String? = null
+data class BookResponseDTO(
+    var isbn: String?,
+    var requestedIsbn: String? = null,
+    var title: String? = null,
+    var subtitle: String? = null,
+    var combinedTitle: String? = null,
+    var author: String? = null,
+    var year: String? = null,
+    var edition: String? = null,
+    var publisher: String? = null,
+    var place: String? = null,
+    var description: String? = null,
+    var source: String? = null,
+    var series: String? = null,
+    var volume: String? = null,
+    var price: String? = null,
+    var pages: String? = null,
+    var tableOfContentsUrl: String? = null,
+    var abstractUrl: String? = null,
+    var abstract: String? = null,
     var error: String? = null
-}
+)
