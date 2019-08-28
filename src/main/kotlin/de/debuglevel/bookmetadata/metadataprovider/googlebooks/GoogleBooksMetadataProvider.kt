b6bbd2236b1697aeefa6c82d6eccf6453b1424af
Marc Kohaupt <debuglevel@gmail.com>
2019-08-28 23:30:48 +0200

    Use JsonPath instead of JsonObject chains.
    JsonObject is cumbersome to extract a property; JsonPath provides a simple way to define the path to a property and then extract it.

--- src/main/kotlin/de/debuglevel/bookmetadata/metadataprovider/googlebooks/GoogleBooksMetadataProvider.kt
+++ src/main/kotlin/de/debuglevel/bookmetadata/metadataprovider/googlebooks/GoogleBooksMetadataProvider.kt
@@ -1,13 +1,16 @@
 package de.debuglevel.bookmetadata.metadataprovider.googlebooks
 
-import com.google.gson.Gson
-import com.google.gson.JsonObject
+import com.jayway.jsonpath.Configuration
+import com.jayway.jsonpath.JsonPath
+import com.jayway.jsonpath.Option
+import com.jayway.jsonpath.ParseContext
 import de.debuglevel.bookmetadata.BookResponseDTO
 import de.debuglevel.bookmetadata.NameUtils
 import de.debuglevel.bookmetadata.metadataprovider.BookNotFoundException
 import de.debuglevel.bookmetadata.metadataprovider.MetadataProvider
 import mu.KotlinLogging
 import javax.inject.Singleton
+
 
 @Singleton
 class GoogleBooksMetadataProvider(
@@ -17,75 +20,79 @@ class GoogleBooksMetadataProvider(
 
     override val name = "Google Books"
 
+    private final val jsonParser: ParseContext
+
+    init {
+        val configuration = Configuration.ConfigurationBuilder()
+            .options(Option.DEFAULT_PATH_LEAF_TO_NULL)
+            .build()
+
+        jsonParser = JsonPath.using(configuration)
+    }
+
     override fun toBook(data: String): BookResponseDTO {
         logger.debug("Extracting information from Google Books API JSON...")
 
-        val book = BookResponseDTO(null)
+        val json = jsonParser.parse(data)
 
-        // TODO: maybe use Jackson instead of GSON because it's already included in dependencies
-        val jsonObject = Gson().fromJson(data, JsonObject::class.java)
+        val title = json.read<String>("$.items[0].volumeInfo.title")
+        val subtitle = json.read<String>("$.items[0].volumeInfo.subtitle")
+        val publishDate = json.read<String>("$.items[0].volumeInfo.publishedDate")
+        val year = publishDate?.split("-")?.first()
+        val bookCount = json.read<Int>("$.totalItems")
+        val authorsFirstnameLastname = json.read<List<String>>("$.items[0].volumeInfo.authors[*]")
+        val authorsLastnameFirstname = authorsFirstnameLastname
+            .map {
+                NameUtils.convertToLastnameFirst(it)
+            }
+        val authors = authorsLastnameFirstname.joinToString("; ")
+        val isbns =
+            json.read<List<String>>("$.items[0].volumeInfo.industryIdentifiers[?(@.type == 'ISBN_13')].identifier")
+        val isbn = isbns.firstOrNull()
+        val publisher = json.read<String>("$.items[0].volumeInfo.publisher")
+        val description = json.read<String>("$.items[0].volumeInfo.description")
+        val pages = json.read<Int>("$.items[0].volumeInfo.pageCount")
+        val language = json.read<String>("$.items[0].volumeInfo.language")
+        val textSnippet = json.read<String>("$.items[0].searchInfo.textSnippet")
 
-        val bookCount = jsonObject?.getAsJsonPrimitive("totalItems")?.asInt ?: 0
+        val abstract = listOf(description, textSnippet)
+            .filter { !it.isNullOrBlank() }
+            .joinToString("\n\n")
+
+        val combinedTitle = listOf(title, subtitle)
+            .filter { !it.isNullOrBlank() }
+            .joinToString(": ")
+
         if (bookCount == 0) {
             throw BookNotFoundException()
         }
 
-        book.title = jsonObject?.getAsJsonArray("items")?.get(0)
-                ?.asJsonObject?.getAsJsonObject("volumeInfo")
-                ?.get("title")?.asString
+        val book = BookResponseDTO(null).apply {
+            this.title = title
+            this.subtitle = subtitle
+            this.combinedTitle = combinedTitle
+            this.author = authors
+            this.isbn = isbn
 
-        val subtitle = jsonObject?.getAsJsonArray("items")?.get(0)
-                ?.asJsonObject?.getAsJsonObject("volumeInfo")
-                ?.get("subtitle")?.asString
+//            this.edition = edition
+            this.year = year
 
-        if (!subtitle.isNullOrBlank()) {
-            book.combinedTitle += ": $subtitle"
-        } else {
-            book.combinedTitle = book.title
+            this.publisher = publisher
+//            this.place = place
+
+            this.language = language
+
+//            this.volume = volume
+//            this.series = series
+
+            this.pages = pages.toString()
+//            this.price = price
+
+            this.abstract = abstract
+//            this.abstractUrl = abstractUrl
+
+//            this.tableOfContentsUrl = tableOfContentsUrl
         }
-
-        val author = jsonObject?.getAsJsonArray("items")?.get(0)
-                ?.asJsonObject?.getAsJsonObject("volumeInfo")
-                ?.getAsJsonArray("authors")
-                ?.joinToString(separator = ", ", transform = { it.asJsonPrimitive.asString })
-        book.author = if (author != null) NameUtils.convertToLastnameFirst(author) else null
-
-        val date = jsonObject?.getAsJsonArray("items")?.get(0)
-                ?.asJsonObject?.getAsJsonObject("volumeInfo")
-                ?.get("publishedDate")?.asString
-        // convert "2017-12-24" to "2017"
-        book.year = date?.split("-")?.first()
-
-        // sometimes missing
-        book.isbn = jsonObject?.getAsJsonArray("items")?.get(0)
-                ?.asJsonObject?.getAsJsonObject("volumeInfo")
-            ?.get("industryIdentifiers")
-            ?.asJsonArray?.firstOrNull { it?.asJsonObject?.get("type")?.asString == "ISBN_13" }
-                ?.asJsonObject?.get("identifier")?.asString
-
-        book.publisher = jsonObject?.getAsJsonArray("items")?.get(0)
-                ?.asJsonObject?.getAsJsonObject("volumeInfo")
-                ?.get("publisher")?.asString
-
-        val description = jsonObject?.getAsJsonArray("items")?.get(0)
-                ?.asJsonObject?.getAsJsonObject("volumeInfo")
-                ?.get("description")?.asString
-
-        book.pages = jsonObject?.getAsJsonArray("items")?.get(0)
-            ?.asJsonObject?.getAsJsonObject("volumeInfo")
-            ?.get("pageCount")?.asString
-
-        book.language = jsonObject?.getAsJsonArray("items")?.get(0)
-            ?.asJsonObject?.getAsJsonObject("volumeInfo")
-            ?.get("language")?.asString
-
-        val textSnippet = jsonObject?.getAsJsonArray("items")?.get(0)
-            ?.asJsonObject?.getAsJsonObject("searchInfo")
-            ?.get("textSnippet")?.asString
-
-        book.abstract = listOf(description, textSnippet)
-            .filter { !it.isNullOrBlank() }
-            .joinToString("\n\n")
 
         logger.debug("Extracted information from Google Books API JSON: $book")
         return book
--- src/main/kotlin/de/debuglevel/bookmetadata/metadataprovider/openlibrary/OpenLibraryMetadataProvider.kt
+++ src/main/kotlin/de/debuglevel/bookmetadata/metadataprovider/openlibrary/OpenLibraryMetadataProvider.kt
@@ -2,6 +2,10 @@ package de.debuglevel.bookmetadata.metadataprovider.openlibrary
 
 import com.google.gson.Gson
 import com.google.gson.JsonObject
+import com.jayway.jsonpath.Configuration
+import com.jayway.jsonpath.JsonPath
+import com.jayway.jsonpath.Option
+import com.jayway.jsonpath.ParseContext
 import de.debuglevel.bookmetadata.BookResponseDTO
 import de.debuglevel.bookmetadata.NameUtils
 import de.debuglevel.bookmetadata.metadataprovider.BookNotFoundException
@@ -17,56 +21,83 @@ class OpenLibraryMetadataProvider(
 
     override val name = "OpenLibrary"
 
+    private final val jsonParser: ParseContext
+
+    init {
+        val configuration = Configuration.ConfigurationBuilder()
+            .options(Option.DEFAULT_PATH_LEAF_TO_NULL)
+            .build()
+
+        jsonParser = JsonPath.using(configuration)
+    }
+
     override fun toBook(data: String): BookResponseDTO {
         logger.debug("Extracting information from OpenLibrary Books API JSON...")
 
-        val book = BookResponseDTO(null)
-
-        // TODO: maybe use Jackson instead of GSON because it's already included in dependencies
-        val jsonObject = Gson().fromJson(data, JsonObject::class.java)
-
-        val mainElement =
-            if (jsonObject?.keySet()?.firstOrNull() != null) jsonObject.getAsJsonObject(jsonObject.keySet()?.first()) else null
-
-        if (mainElement == null || jsonObject?.isJsonNull == true) {
-            throw BookNotFoundException()
-        }
-
-        book.title = mainElement.getAsJsonPrimitive("title")?.asString
-
-        val subtitle = mainElement.getAsJsonPrimitive("subtitle")?.asString
-
-        if (!subtitle.isNullOrBlank()) {
-            book.combinedTitle += ": $subtitle"
-        } else {
-            book.combinedTitle = book.title
-        }
-
-        book.author = mainElement.getAsJsonArray("authors")
-            ?.joinToString(separator = "; ",
-                transform = {
-                    val name = it.asJsonObject.getAsJsonPrimitive("name").asString
-                    val convertedName = NameUtils.convertToLastnameFirst(name)
-                    convertedName
+        // OpenLibrary uses a ridiculous format where the key contains a value: { "ISBN:9783551551672": { ... }}"
+        // this extracts the value of this key and converts it back to JSON for JsonPath processing
+        val mainElementJson = run {
+            // TODO: maybe use Jackson instead of GSON because it's already included in dependencies
+            val jsonObject = Gson().fromJson(data, JsonObject::class.java)
+            val mainElement =
+                if (jsonObject?.keySet()?.firstOrNull() != null) {
+                    jsonObject.getAsJsonObject(jsonObject.keySet()?.first())
+                } else {
+                    null
                 }
-            )
+            if (mainElement == null || jsonObject?.isJsonNull == true) {
+                throw BookNotFoundException()
+            }
+            mainElement.toString()
+        }
 
-        book.year = mainElement.getAsJsonPrimitive("publish_date")?.asString
+        val json = jsonParser.parse(mainElementJson)
 
-        book.isbn = mainElement.getAsJsonObject("identifiers")
-            ?.getAsJsonArray("isbn_13")?.get(0)
-            ?.asString
+        val title = json.read<String>("$.title")
+        val subtitle = json.read<String>("$.subtitle")
+        val year = json.read<String>("$.publish_date")
+        val authorsFirstnameLastname = json.read<List<String>>("$.authors[*].name")
+        val authorsLastnameFirstname = authorsFirstnameLastname
+            .map {
+                NameUtils.convertToLastnameFirst(it)
+            }
+        val authors = authorsLastnameFirstname.joinToString("; ")
+        val isbn = json.read<String>("$.identifiers.isbn_13[0]")
+        val publishers = json.read<List<String>>("$.publishers[*].name")
+        val publisher = publishers.joinToString(", ")
+        val place = json.read<String>("$.publish_places[0].name")
+        val pages = json.read<Int>("$.number_of_pages")
 
-        book.place = mainElement.getAsJsonArray("publish_places")
-            ?.get(0)
-            ?.asJsonObject
-            ?.getAsJsonPrimitive("name")
-            ?.asString
+        val combinedTitle = listOf(title, subtitle)
+            .filter { !it.isNullOrBlank() }
+            .joinToString(": ")
 
-        book.publisher = mainElement.getAsJsonArray("publishers")
-            ?.joinToString(separator = ", ", transform = { it.asJsonObject.getAsJsonPrimitive("name").asString })
+        val book = BookResponseDTO(null).apply {
+            this.title = title
+            this.subtitle = subtitle
+            this.combinedTitle = combinedTitle
+            this.author = authors
+            this.isbn = isbn
 
-        book.pages = mainElement.getAsJsonPrimitive("number_of_pages")?.asString
+//            this.edition = edition
+            this.year = year
+
+            this.publisher = publisher
+            this.place = place
+
+//            this.language = language
+
+//            this.volume = volume
+//            this.series = series
+
+            this.pages = pages.toString()
+//            this.price = price
+
+//            this.abstract = abstract
+//            this.abstractUrl = abstractUrl
+
+//            this.tableOfContentsUrl = tableOfContentsUrl
+        }
 
         logger.debug("Extracted information from OpenLibrary Books API JSON: $book")
         return book
