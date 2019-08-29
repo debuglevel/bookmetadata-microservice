dd2979d8ab6554d5221ad979e83273e0b65060ec
Marc Kohaupt <debuglevel@gmail.com>
2019-08-29 17:34:45 +0200

    Do not threat things in parentheses as lastname

--- src/main/kotlin/de/debuglevel/bookmetadata/NameUtils.kt
+++ src/main/kotlin/de/debuglevel/bookmetadata/NameUtils.kt
@@ -6,18 +6,31 @@ object NameUtils {
      * This function will produce errors on any multipart lastname (e.g. "Friedemann Schulz von Thun" becomes "Thun, Friedemann Schulz von" instead of "Schulz von Thun, Friedemann")
      * If the given String contains a comma, it's assumed that there are multiple names given ("Alan Turing, Ada Lovelace") which will be converted to "Turing, Alan; Lovelace, Ada"
      */
-    fun convertToLastnameFirst(name: String): String {
-        val regex = Regex("[,;] *")
+    fun convertToLastnameFirst(name: String?): String? {
+        if (name.isNullOrBlank()) {
+            return null
+        }
+
+        val regex = Regex(" *[,;] *")
         return name.split(regex)
+            .map { it.trim() }
             .map { convertOne(it) }
             .joinToString("; ")
     }
 
     private fun convertOne(name: String): String {
         val parts = name.split(" ")
-        val lastname = parts.last()
-        val firstname = parts.take(parts.count() - 1).joinToString(" ")
 
-        return "$lastname, $firstname"
+        // Search the last word which is not something like "(Hrsg.)" and take it as the lastname.
+        val parenthesesRegex = Regex("""[(\[].*[)\]]""")
+        val lastname = parts.last { !it.matches(parenthesesRegex) }
+        // Then remove the last occurence of "lastname" in the parts and concat everything
+        val rest = parts
+            .reversed()
+            .minus(lastname)
+            .reversed()
+            .joinToString(" ")
+
+        return "$lastname, $rest"
     }
 }
--- src/test/kotlin/de/debuglevel/bookmetadata/NameUtilsTests.kt
+++ src/test/kotlin/de/debuglevel/bookmetadata/NameUtilsTests.kt
@@ -14,10 +14,13 @@ class NameUtilsTests {
         "'H. P. Lovecraft', 'Lovecraft, H. P.'",
         "'Horst von und zu Jenem', 'Jenem, Horst von und zu'",
         "'Friedemann Schulz von Thun', 'Thun, Friedemann Schulz von'",
+        "'Walter Moers (Hrsg.)', 'Moers, Walter (Hrsg.)'",
+
         "'Alan Turing, Ada Lovelace', 'Turing, Alan; Lovelace, Ada'",
         "'Alan Turing,Ada Lovelace', 'Turing, Alan; Lovelace, Ada'",
         "'Alan Turing,  Ada Lovelace', 'Turing, Alan; Lovelace, Ada'",
-        "'Alan Turing; Ada Lovelace', 'Turing, Alan; Lovelace, Ada'"
+        "'Alan Turing; Ada Lovelace', 'Turing, Alan; Lovelace, Ada'",
+        "'Friedemann Schulz von Thun ; Dagmar Kumbier (Hg.)', 'Thun, Friedemann Schulz von; Kumbier, Dagmar (Hg.)'"
     )
     fun `parse data from provider`(value: String, expected: String) {
         // Arrange
